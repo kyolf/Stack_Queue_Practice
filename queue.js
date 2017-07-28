4ebb0aa6ad226c9f73bdd2aa833f088adf3667a6
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-07-28 08:41:34 -0700

    finish stack and queue implementation

--- queue.js
+++ queue.js
@@ -1,5 +1,83 @@
 'use strict';
 
-class Queue{
-
+function createNodeQueue(data,next=null, prev=null){
+  return ({
+    data,
+    next,
+    prev
+  });
 }
+//Queue order
+//last <- node <- first
+class Queue{
+  constructor(){
+    this.first = null;
+    this.last = null;
+  }
+
+  enqueue(value){
+    const node = createNodeQueue(value);
+    if(this.last){
+      node.next = this.last;
+      this.last.prev = node;
+    }
+
+    this.last = node;
+
+    if(this.first === null){
+      this.first = node;  
+    }
+  }
+
+  dequeue(){
+    if(this.first=== null){
+      throw new Error('Can not dequeue Empty List');
+    }
+
+    const node = this.first;
+    this.first = node.prev;
+    this.first.next = null;
+    
+    if(node === this.last){
+      this.last === null;
+    }
+
+    return node.data;
+  }
+}
+
+function displayQueue(queue){
+  if(queue.first === null){
+    console.log('Empty list');
+    return;
+  }
+  let node = queue.first;
+  while(node.prev !== null){
+    console.log(node.data);
+    node = node.prev;
+  }
+  console.log(node.data);
+}
+
+console.log('////////////////////////');
+console.log('Queue');
+console.log('////////////////////////');
+const q = new Queue();
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+
+console.log('////////////////////////');
+console.log('Queue Dequeue');
+console.log('////////////////////////');
+console.log(q.dequeue());
+
+console.log('////////////////////////');
+console.log('Queue After dequeue');
+console.log('////////////////////////');
+console.log(q);
+
+console.log('////////////////////////');
+console.log('Queue Display');
+console.log('////////////////////////');
+console.log(displayQueue(q));
--- stack.js
+++ stack.js
@@ -1,5 +1,80 @@
 'use strict';
 
-class Stack{
-  
+function createNodeStack(data, next=null){
+  return ({
+    data,
+    next
+  });
 }
+
+class Stack{
+  constructor(){
+    this.top = null;
+  }
+
+  push(value){
+    if(this.top === null){
+      this.top = createNodeStack(value);
+      return;
+    }
+    const node = createNodeStack(value, this.top);
+    this.top = node;
+  }
+
+  pop(){
+    if(this.top === null){
+      throw new Error('Can not pop empty list');
+    }
+    const node = this.top;
+    this.top = node.next;
+    return node.data;
+  }
+}
+
+function peek(stack){
+  if(stack.top === null){
+    throw new Error('Empty list');
+  }
+  return stack.top.data;
+}
+
+function displayStack(stack){
+  let node = stack.top;
+  if(node === null){
+    console.log('Empty List');
+    return;
+  }
+
+  while(node.next !== null){
+    console.log(node.data);
+    node = node.next;
+  }
+  console.log(node.data);
+}
+
+console.log('////////////////////////');
+console.log('Stack');
+console.log('////////////////////////');
+const s = new Stack();
+s.push(1);
+s.push(2);
+s.push(3);
+console.log('////////////////////////');
+console.log('Stack Pop');
+console.log('////////////////////////');
+console.log(s.pop());
+
+console.log('////////////////////////');
+console.log('Stack after pop');
+console.log('////////////////////////');
+console.log(s);
+
+console.log('////////////////////////');
+console.log('Stack Peek');
+console.log('////////////////////////');
+console.log(peek(s));
+
+console.log('////////////////////////');
+console.log('Stack Display');
+console.log('////////////////////////');
+console.log(displayStack(s));
